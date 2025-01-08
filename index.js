1094a64d9342dbde0e05e5002de9c1417434737c
bansi-hkapps <bansikhokhariya59@gmail.com>
2025-01-08 12:42:51 +0530

    123

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const request = require('request');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer-core');
+// const chromium = require('chrome-aws-lambda');
 
 app.use(cors()); // Enable CORS for all requests
 
@@ -28,7 +30,15 @@ app.get('/fetch-facebookads-data', async (req, res) => {
     }
 
     try {
-        const browser = await puppeteer.launch();
+
+        const browser = await puppeteer.launch({
+            headless: true,
+            args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            ],
+            });
+
         const page = await browser.newPage();
 
         await page.goto(url, { waitUntil: 'networkidle2' }); // Ensure the page is fully loaded
@@ -64,6 +74,57 @@ app.get('/fetch-facebookads-data', async (req, res) => {
         res.status(500).send('Error fetching or processing the URL');
     }
 });
+
+
+
+
+// app.get('/fetch-facebookads-data', async (req, res) => {
+//     const url = req.query.url;
+
+//     if (!url) {
+//         return res.status(400).send('URL is required');
+//     }
+
+//     let browser;
+//     try {
+//         // Launch Puppeteer with chrome-aws-lambda configuration
+//         browser = await puppeteer.launch({
+//             args: [...chromium.args, '--no-sandbox', '--disable-setuid-sandbox'],
+//             executablePath: await chromium.executablePath, // Use chromium's executable path
+//             headless: chromium.headless,                  // Ensure headless mode
+//         });
+
+//         const page = await browser.newPage();
+
+//         // Navigate to the page and wait for it to load
+//         await page.goto(url, { waitUntil: 'networkidle2' });
+
+//         // Extracting data
+//         const modalData = await page.evaluate(() => {
+//             const linkElement = document.querySelector('.x1hl2dhg.x1lku1pv.x8t9es0.x1fvot60.xxio538.xjnfcd9.xq9mrsl.x1yc453h.x1h4wwuj.x1fcty0u.x1lliihq');
+//             const pageNameElement = document.querySelector('.x8t9es0.x1fvot60.xxio538.x108nfp6.xq9mrsl.x1h4wwuj.x117nqv4.xeuugli');
+//             const jobTextElement = document.querySelector('span.x8t9es0.xw23nyj.xo1l8bm.x63nzvj.x108nfp6.xq9mrsl.x1h4wwuj.xeuugli div._4ik4._4ik5 span');
+
+//             return {
+//                 pageName: pageNameElement ? pageNameElement.innerText : null,
+//                 jobText: jobTextElement ? jobTextElement.innerText : null,
+//                 linkText: linkElement ? linkElement.innerText : null,
+//                 linkUrl: linkElement ? linkElement.href : null,
+//             };
+//         });
+
+//         // Close the browser instance
+//         await browser.close();
+
+//         // Send the extracted data as JSON
+//         res.json({ status: 'success', modalData });
+//     } catch (error) {
+//         console.error(error);
+//         if (browser) await browser.close();
+//         res.status(500).send('Error fetching or processing the URL');
+//     }
+// });
+
 
 
 app.listen(PORT, () => {
